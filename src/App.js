f39e79c2c6e06e24ab66eb5fece3a43b8e563935
rexdougie <bitokdouglas@gmail.com>
2021-10-18 15:45:02 +0100

    CSS file added and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (<div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias enim, voluptatem laborum officiis odio quaerat ut minus iusto nobis omnis quisquam, et, animi quo praesentium id minima magni exercitationem.</p>
+    </div>)
+};
 
 export default App;
