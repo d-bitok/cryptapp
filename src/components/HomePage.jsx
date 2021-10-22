4ac0fd72173ecd2b317a9f9989bfa2b98906f095
rexdougie <bitokdouglas@gmail.com>
2021-10-22 16:48:35 +0100

    Coinanking API created and Connected

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,44 @@
 import React from "react";
+import millify from "millify";
+import { 
+    // Topography, 
+    Row, 
+    Col, 
+    Statistic, 
+    Typography 
+} from "antd";
+// import { Link } from "react-router-dom";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const { Title } = Typography;
 
 const HomePage = () => {
+    const { data, isFetching } = useGetCryptosQuery(10);
+    const globalStats = data?.data?.stats;
+
+    
+    if (isFetching) console.log('Fetching ...');
+
     return (
-        <div>
-            <p>
-                HomePage
-            </p>
-        </div>
+        <>
+        <Title level={2} className="heading">Global Crypto Stats</Title>
+        <Row gutter={[32, 32]}>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+            {/* <Col span={12}><Statistic title="Total Exchanges" value={"3"} /></Col> */}
+            {/* <Col span={12}><Statistic title="Total Market Cap" value="3" /></Col> */}
+            {/* <Col span={12}><Statistic title="Total 24h Volume" value="3" /></Col> */}
+            {/* <Col span={12}><Statistic title="Total Markets" value="3" /></Col> */}
+            {/* <Col span={12}><Statistic title="Total Cryptocurrencies" value="3" /></Col> */}
+
+            <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+            <Col span={12}><Statistic title="Total Market Cap" value={`KES ${millify(globalStats.totalMarketCap)}`} /></Col>
+            <Col span={12}><Statistic title="Total 24h Volume" value={`KES ${millify(globalStats.total24hVolume)}`} /></Col>
+            <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value="3" /></Col>
+{/*  */}
+        </Row>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App'
-import 'antd/dist/antd.css'
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
+  <React.StrictMode>
     <Router>
+      <Provider store={store}>
         <App />
-    </Router>,
-    document.getElementById('root'),
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
